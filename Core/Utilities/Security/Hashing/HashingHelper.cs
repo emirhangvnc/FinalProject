e18bad7c5b78c4aeecba8a42e7aa95b9f44d91c7
emirhanguvenc <emirhanguvenc07@gmail.com>
2022-05-18 15:19:39 +0300

    Commit

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        public static void CreatePasswordHash(string password,out byte[] passwordHash, out byte[] passwordSalt) // Boşta olsa içini doldurup değer döndürmeye yarar
+        {
+            using (var hmac =new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)); //byte çevirme
+
+            }
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+              var computedHash=  passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i]!=passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
